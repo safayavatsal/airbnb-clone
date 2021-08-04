1cacb327ad76481892c948cd643b0f5267e69429
Vatsal Safaya <safayavatsal@Vatsals-MacBook-Air.local>
2021-08-05 01:31:55 +0530

    day 1 build

---
+++ components/Footer.js
@@ -0,0 +1,44 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">About</h5>
+                <p>How AirBnB works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>AirBnB Plus</p>
+                <p>AirBnB Luxe</p>
+
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>TheCode5 fam</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>TheCode5</p>
+                <p>Presents</p>
+                <p>Beginners to advanced PYTHON course</p>
+                <p>Helped Hundreds</p>
+                <p>Join Now</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Center</p>
+                <p>Trust & Safety</p>
+                <p>Say Hi Fam</p>
+                <p>Easter Eggs</p>
+                <p>For the win</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
